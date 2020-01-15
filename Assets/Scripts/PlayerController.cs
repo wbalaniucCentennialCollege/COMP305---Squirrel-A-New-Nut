e0eeac12d5cae451ecd527f5484dfc99d79a27cb
Wallace <wbalaniu@my.centennialcollege.ca>
2020-01-15 16:25:34 -0500

    Started movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // "Public" variables
     [SerializeField]
     private float speed = 10.0f;
+    [SerializeField]
+    private float jumpForce = 500.0f;
 
+    // Private variables
+    private bool isGrounded = false;
     private Rigidbody2D rBody;
 
     // Start is called before the first frame update
@@ -22,7 +27,24 @@ public class PlayerController : MonoBehaviour
         float horiz = Input.GetAxis("Horizontal");
 
         // Jumping code will go here!
+        if(isGrounded && Input.GetAxis("Jump") > 0 && Mathf.Approximately(rBody.velocity.y, 0.0f))
+        {
+            rBody.AddForce(new Vector2(0.0f, jumpForce));
+            isGrounded = false;
+        }
 
         rBody.velocity = new Vector2(horiz * speed, rBody.velocity.y);
+
+        Debug.Log("Y velocity: " + rBody.velocity.y);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        // Check if the object the player is colliding with is tagged ground
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            Debug.Log("I am on the ground");
+        }
     }
 }
