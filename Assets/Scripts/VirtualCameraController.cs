3ba8ccc9923f2670b29f5260bff43b19e6d6a216
Wallace Balaniuc <wbalaniu@my.centennialcollege.ca>
2020-01-29 14:41:39 -0500

    Camera Is Better

---
+++ Assets/Scripts/VirtualCameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+[ExecuteInEditMode]
+public class VirtualCameraController : MonoBehaviour
+{
+    public List<GameObject> virtualCameras;
+
+    void Start()
+    {
+        virtualCameras.Clear();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            virtualCameras.Add(transform.GetChild(i).gameObject);
+        }
+    }
+
+    public void TransitionTo(GameObject cameraToTransitionTo)
+    {
+        foreach (GameObject g in virtualCameras)
+        {
+            if(g == cameraToTransitionTo)
+            {
+                g.GetComponent<CinemachineVirtualCamera>().Priority = 10;
+            }
+            else
+            {
+                g.GetComponent<CinemachineVirtualCamera>().Priority = 5;
+            }
+        }
+    }
+}
