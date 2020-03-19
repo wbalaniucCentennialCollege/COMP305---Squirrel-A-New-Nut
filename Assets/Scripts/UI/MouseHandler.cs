878495aa901a581d4540e74824c30823603af624
Wallace <wbalaniu@my.centennialcollege.ca>
2020-03-19 15:32:42 -0400

    Added audio

---
+++ Assets/Scripts/UI/MouseHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHandler : MonoBehaviour
+{
+    private AudioSource aSrc;
+
+    private void Start()
+    {
+        aSrc = GetComponent<AudioSource>();
+    }
+
+    public void MouseHover()
+    {
+        aSrc.Play();
+    }
+}
