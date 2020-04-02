8b02755cb2218754ac30f4b5ff2b8af1755843fa
Wallace <Balaniuc>
2020-04-02 11:08:21 -0400

    Moving Platform

---
+++ Assets/Scripts/Editor/MovingPlatformEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(MovingPlatform))]
+public class MovingPlatformEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MovingPlatform plat = (MovingPlatform)target;
+
+        if (GUILayout.Button("Add Waypoint"))
+        {
+            plat.AddWaypoint();
+        }
+
+        EditorGUILayout.BeginVertical();
+
+        for(int i = 0; i < plat.Waypoints.Count; i++)
+        {
+            plat.Waypoints[i].position = EditorGUILayout.Vector3Field("Waypoint " + i, plat.Waypoints[i].position);
+        }
+
+        if(GUILayout.Button("Reset"))
+        {
+            plat.Reset();
+        }
+    }
+}
---
+++ Assets/Scripts/Level/MovingPlatform.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    private List<Transform> waypoints;
+
+    public List<Transform> Waypoints
+    {
+        get
+        {
+            return waypoints;
+        }
+    }
+
+    private void Awake()
+    {
+        waypoints = new List<Transform>();
+    }
+
+    public void AddWaypoint()
+    {
+        GameObject g = new GameObject();
+        g.transform.position = transform.position;
+        g.transform.parent = transform.parent.GetChild(1).transform;
+        g.name = waypoints.Count.ToString();
+        waypoints.Add(g.transform);
+    }
+
+    public void Reset()
+    {
+        waypoints.Clear();
+        List<GameObject> children = new List<GameObject>();
+        foreach (Transform child in transform.parent.GetChild(1))
+        {
+            children.Add(child.gameObject);
+        }
+        children.ForEach(child => DestroyImmediate(child));
+    }
+
+    /*
+    private void OnDrawGizmos()
+    {
+        if (waypoints.Count > 0)
+        {
+            for (int i = 0; i < waypoints.Count; i++)
+            {
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawWireSphere(waypoints[i].position, 0.1f);
+            }
+        }
+    }
+    */
+}
